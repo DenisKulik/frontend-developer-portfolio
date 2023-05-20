9fd4961a46432a8266da2da0047003b635df5caa
Denis Kulik <kulikdenis1996x@gmail.com>
2023-05-20 20:04:14 +0700

    move Header to layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { AppWrapper } from './AppStyles.ts';
-import Header from './components/Header/Header.tsx';
+import Header from './layout/Header/Header.tsx';
 
 const App = () => {
     return (
