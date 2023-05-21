b943751c84d8879c9816edb38d26bfd8f406041f
Denis Kulik <kulikdenis1996x@gmail.com>
2023-05-21 17:40:53 +0700

    add fixed position for header

--- src/layout/Header/HeaderStyles.ts
+++ src/layout/Header/HeaderStyles.ts
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
 export const HeaderWrapper = styled.header`
+  width: 100%;
+  position: fixed;
+  top: 0;
   padding: 2rem 0;
+  z-index: 9999;
 `;
 
 export const HeaderMenu = styled.div`
