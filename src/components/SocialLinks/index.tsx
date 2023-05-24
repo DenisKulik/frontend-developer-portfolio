084baadc0a46cac00e5725f0ab6fd1e210c1959d
Denis Kulik <kulikdenis1996x@gmail.com>
2023-05-24 18:42:19 +0700

    replace icons from React icons with Icon component

--- src/components/SocialLinks/index.tsx
+++ src/components/SocialLinks/index.tsx
@@ -1,22 +1,38 @@
 import { IconLink, SocialLinksWrapper } from './SocialLinks.ts';
+import { useTheme } from 'styled-components';
 import {
     RiGithubFill, RiLinkedinBoxFill, RiTelegramFill
 } from 'react-icons/ri';
+import Icon from '../Icon';
 
 const SocialLinks = () => {
+    const theme = useTheme();
+
     return (
         <SocialLinksWrapper>
             <IconLink href="https://github.com/DenisKulik" target="_blank">
-                <RiGithubFill />
+                <Icon
+                    icon={RiGithubFill}
+                    size="2rem"
+                    color={theme?.colors.primaryLight}
+                />
             </IconLink>
             <IconLink
                 href="https://www.linkedin.com/in/denis-kulik"
                 target="_blank"
             >
-                <RiLinkedinBoxFill />
+                <Icon
+                    icon={RiLinkedinBoxFill}
+                    size="2rem"
+                    color={theme?.colors.primaryLight}
+                />
             </IconLink>
             <IconLink href="https://t.me/kulikdp" target="_blank">
-                <RiTelegramFill />
+                <Icon
+                    icon={RiTelegramFill}
+                    size="2rem"
+                    color={theme?.colors.primaryLight}
+                />
             </IconLink>
         </SocialLinksWrapper>
     );
