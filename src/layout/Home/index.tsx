247f34815766472182f8a4551ba4f7e9115c22ff
Denis Kulik <kulikdenis1996x@gmail.com>
2023-07-19 19:40:06 +0700

    update customed particles for Home background

--- src/layout/Home/index.tsx
+++ src/layout/Home/index.tsx
@@ -3,12 +3,95 @@ import type {
     Container as ParticlesContainer, Engine
 } from 'tsparticles-engine';
 import { loadSlim } from 'tsparticles-slim';
+import type { ISourceOptions } from 'tsparticles-engine';
 import {
     AboutMe, CustomedParticles, Description, HomeInner,
     HomeWrapper, MyPhoto, PhotoWrapper, Subtitle, Title,
 } from './HomeStyles.ts';
 import photo from '../../assets/my-photo.jpg';
 import Container from '../Container';
+
+const particlesOptions: ISourceOptions = {
+    fullScreen: false,
+    particles: {
+        number: {
+            value: 100,
+            density: {
+                enable: true,
+            },
+        },
+        color: {
+            value: 'rgb(223, 157, 200)',
+        },
+        shape: {
+            type: 'circle',
+        },
+        opacity: {
+            value: {
+                min: 0.1,
+                max: 0.2,
+            },
+            animation: {
+                enable: true,
+                speed: 2,
+                sync: false,
+            },
+        },
+        size: {
+            value: {
+                min: 1,
+                max: 9,
+            },
+            animation: {
+                enable: true,
+                speed: 10,
+                sync: false,
+            },
+        },
+        links: {
+            enable: true,
+            distance: 150,
+            color: 'rgb(170, 54, 124)',
+            opacity: 0.4,
+            width: 1,
+        },
+        move: {
+            enable: true,
+            speed: 2,
+        },
+    },
+    interactivity: {
+        events: {
+            onHover: {
+                enable: true,
+                mode: 'slow',
+                parallax: {
+                    enable: true,
+                    smooth: 10,
+                    force: 50,
+                },
+            },
+            onClick: {
+                enable: true,
+                mode: 'push',
+            },
+        },
+        modes: {
+            grab: {
+                distance: 400,
+                links: {
+                    opacity: 0.8,
+                },
+            },
+            push: {
+                quantity: 4,
+            },
+        },
+    },
+    background: {
+        color: 'rgb(18, 18, 18)',
+    },
+};
 
 const Home = () => {
     const particlesInit = useCallback(async (engine: Engine) => {
@@ -25,95 +108,7 @@ const Home = () => {
             <CustomedParticles
                 init={particlesInit}
                 loaded={particlesLoaded}
-                options={{
-                    fullScreen: false,
-                    particles: {
-                        groups: {
-                            z5000: {
-                                number: {
-                                    value: 70,
-                                },
-                                zIndex: {
-                                    value: 50,
-                                },
-                            },
-                            z7500: {
-                                number: {
-                                    value: 30,
-                                },
-                                zIndex: {
-                                    value: 75,
-                                },
-                            },
-                            z2500: {
-                                number: {
-                                    value: 50,
-                                },
-                                zIndex: {
-                                    value: 25,
-                                },
-                            },
-                            z1000: {
-                                number: {
-                                    value: 40,
-                                },
-                                zIndex: {
-                                    value: 10,
-                                },
-                            },
-                        },
-                        number: {
-                            value: 200,
-                        },
-                        color: {
-                            value: '#fff',
-                            animation: {
-                                enable: false,
-                                speed: 20,
-                                sync: true,
-                            },
-                        },
-                        shape: {
-                            type: 'circle',
-                        },
-                        opacity: {
-                            value: 0.8,
-                        },
-                        size: {
-                            value: 3,
-                        },
-                        move: {
-                            angle: {
-                                value: 10,
-                                offset: 0,
-                            },
-                            enable: true,
-                            speed: 1,
-                            direction: 'bottom',
-                        },
-                        zIndex: {
-                            value: 5,
-                            opacityRate: 0.5,
-                        },
-                    },
-                    background: {
-                        color: 'rgb(18, 18, 18)',
-                    },
-                    emitters: {
-                        position: {
-                            y: 55,
-                            x: -5,
-                        },
-                        rate: {
-                            delay: 7,
-                            quantity: 1,
-                        },
-                        size: {
-                            width: 0,
-                            height: 0,
-                        },
-                    },
-                }}
+                options={particlesOptions}
             />
             <Container>
                 <HomeInner>
